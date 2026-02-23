7f87cd1d5ee7d424fa0b1aea353d393604bdc72f
klysa7 <klysandropoulos@gmail.com>
2026-02-24 00:48:43 +0100

    first delete

--- calcite/src/main/java/dk/ku/di/dms/vms/calcite/legacy/GroupByMinMaxEnum.java
+++
@@ -1,5 +0,0 @@
-package dk.ku.di.dms.vms.calcite.legacy;
-
-public enum GroupByMinMaxEnum {
-    MIN, MAX
-}
--- calcite/src/main/java/dk/ku/di/dms/vms/calcite/legacy/runtime/VModbKeys.java
+++
@@ -1,7 +0,0 @@
-package dk.ku.di.dms.vms.calcite.legacy.runtime;
-
-public final class VModbKeys {
-    private VModbKeys() {}
-    public static final String RUNTIME = "vmodb.runtime";
-    public static final String SCHEMA  = "vmodb.schema";
-}
